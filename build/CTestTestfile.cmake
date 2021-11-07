5e6a0170709aea401de74f9d8865b57497c37e2c
thebo8800 <thebo8800@gmail.com>
2021-11-07 17:14:13 -0500

    after fix

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/thebo/OneDrive - The Ohio State University/Useless/Desktop/HACK OH-IO/HACK-OHIO
+# Build directory: C:/Users/thebo/OneDrive - The Ohio State University/Useless/Desktop/HACK OH-IO/HACK-OHIO/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
