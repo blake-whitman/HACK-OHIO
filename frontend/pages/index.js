5f5dfff758811427381d46561e17bfde204dab32
thebo8800 <thebo8800@gmail.com>
2021-11-06 22:42:17 -0400

    Added new chart

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,12 +4,14 @@ export default function Home() {
   return (
     <>
       <main role="main" className="h-full inline w-full sm:w-3/4 md:w-4/5">
-        <Link href="/dashboard"><h1 className="text-grey-dark text-3xl font-bold m-8 ml-12">Dashboard</h1></Link>
+        <h1 className="text-grey-dark text-3xl font-bold m-8 ml-12">Dashboard</h1>
         <div className="ml-10 mr-10 flex flex-wrap gap-8">
           <div className="bg-white shadow-lg rounded-lg h-72 w-72"></div>
           <div className="bg-white shadow-lg rounded-lg h-72 w-72"></div>
           <div className="bg-white shadow-lg rounded-lg h-72 w-72"></div>
           <div className="bg-white shadow-lg rounded-lg h-72 w-72"></div>
+          
+
         </div>
       </main>
     </>
